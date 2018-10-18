938ea8cea5f139393a8034fe08e67618bcac0721
Josh Humphries <j.humphries@nhm.ac.uk>
2018-10-19 00:13:20 +0100

    Use unicode in controller

--- ckanext/versioned_datastore/controllers/datastore.py
+++ ckanext/versioned_datastore/controllers/datastore.py
@@ -7,11 +7,11 @@ class ResourceDataController(base.BaseController):
 
     def resource_data(self, package_name, resource_id):
         try:
-            toolkit.c.pkg_dict = toolkit.get_action('package_show')(None, {'id': package_name})
-            toolkit.c.resource = toolkit.get_action('resource_show')(None, {'id': resource_id})
+            toolkit.c.pkg_dict = toolkit.get_action(u'package_show')(None, {u'id': package_name})
+            toolkit.c.resource = toolkit.get_action(u'resource_show')(None, {u'id': resource_id})
         except logic.NotFound:
-            base.abort(404, _('Resource not found'))
+            base.abort(404, _(u'Resource not found'))
         except logic.NotAuthorized:
-            base.abort(401, _('Unauthorized to edit this resource'))
+            base.abort(401, _(u'Unauthorized to edit this resource'))
 
-        return base.render('package/resource_data.html')
+        return base.render(u'package/resource_data.html')
