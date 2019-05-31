2a47f3d26214118756b69a2fe5a5fdc20a584641
Josh Humphries <j.humphries@nhm.ac.uk>
2019-05-31 13:25:28 +0100

    Add a way of retrieving all resource details objects

--- ckanext/versioned_datastore/lib/details.py
+++ ckanext/versioned_datastore/lib/details.py
@@ -1,4 +1,5 @@
 import json
+from collections import OrderedDict
 
 from ckan import model
 from ckanext.versioned_datastore.model.details import DatastoreResourceDetails
@@ -32,3 +33,27 @@ def get_details(resource_id, version):
         .filter(DatastoreResourceDetails.resource_id == resource_id) \
         .filter(DatastoreResourceDetails.version == version) \
         .first()
+
+
+def get_all_details(resource_id, up_to_version=None):
+    '''
+    Retrieve all the details for a resource and return them as an OrderedDict using the versions as
+    the keys. If the up_to_version parameter is passed then any versions beyond it are ignored and
+    not returned in the resulting OrderedDict.
+
+    :param resource_id: the resource id
+    :param up_to_version: the maximum version to include in the resulting OrderedDict (inclusive)
+    :return: None or an OrderedDict of version: DatastoreResourceDetails objects in ascending order
+    '''
+    query = model.Session.query(DatastoreResourceDetails)\
+        .filter(DatastoreResourceDetails.resource_id == resource_id)\
+        .order_by(DatastoreResourceDetails.version.asc())
+
+    all_details = OrderedDict()
+    for details in query:
+        if up_to_version is not None and details.version > up_to_version:
+            break
+        else:
+            all_details[details.version] = details
+
+    return all_details
