1c28f9482c1572d7f68439fdd128cad6701a69cc
Josh Humphries <j.humphries@nhm.ac.uk>
2021-01-08 16:37:01 +0000

    Add contextlib2 dependency

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'backports.csv==1.0.6',
         'cchardet==2.1.4',
+        u'contextlib2>=0.6.0.post1',
         'openpyxl==2.5.8',
         'requests',
         'six>=1.11.0',
