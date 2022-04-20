0e4da98b3c7b44b08b4579af371555de98d61598
Josh Humphries <j.humphries@nhm.ac.uk>
2022-04-20 21:58:36 +0100

    Add a couple more tests
    Not complete coverage for the xlsx downloader yet but it's better than every other downloader so...

--- tests/test_xlsx_downloader.py
+++ tests/test_xlsx_downloader.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from pathlib import Path
 from unittest.mock import MagicMock
 
@@ -22,11 +23,86 @@ def test_xlsx_single(tmpdir: Path):
 
     workbook = load_workbook(filename=data_file)
     rows = list(workbook[DEFAULT_SHEET_NAME].rows)
+    assert len(rows) == 2
+
     header = list(cell.value for cell in rows[0])
     expected_headers = [RESOURCE_ID_FIELD_NAME] + sorted(data)
     assert header == expected_headers
 
-    for row in rows[1:]:
+    row = rows[1]
+    row_data = dict(zip(header, [cell.value for cell in row]))
+    assert row_data.pop(RESOURCE_ID_FIELD_NAME) == resource_id
+    assert row_data == data
+
+
+def test_xlsx_multiple_one_file(tmpdir: Path):
+    data_a = {'field1': 'A', 'field2': 'B', 'field3': 'C'}
+    data_b = {'field5': 'A', 'field1': 'B', 'field7': 'C'}
+    resource_id_a = 'resource-a'
+    resource_id_b = 'resource-b'
+    field_counts = {
+        resource_id_a: {field: 10 for field in data_a},
+        resource_id_b: {field: 10 for field in data_b},
+    }
+    request = MagicMock(separate_files=False, ignore_empty_fields=False)
+    hit = MagicMock()
+
+    with xlsx_writer(request, tmpdir, field_counts) as write:
+        write(hit, data_a, resource_id_a)
+        write(hit, data_b, resource_id_b)
+
+    data_file = tmpdir / ALL_IN_ONE_FILE_NAME
+    assert data_file.exists()
+
+    workbook = load_workbook(filename=data_file)
+    rows = list(workbook[DEFAULT_SHEET_NAME].rows)
+    header = list(cell.value for cell in rows[0])
+    expected_headers = [RESOURCE_ID_FIELD_NAME] + sorted(set(chain(data_a, data_b)))
+    assert header == expected_headers
+
+    row_a = rows[1]
+    row_a_data = dict(zip(header, [cell.value for cell in row_a]))
+    assert row_a_data.pop(RESOURCE_ID_FIELD_NAME) == resource_id_a
+    assert row_a_data.pop('field5') is None
+    assert row_a_data.pop('field7') is None
+    assert row_a_data == data_a
+
+    row_b = rows[2]
+    row_b_data = dict(zip(header, [cell.value for cell in row_b]))
+    assert row_b_data.pop(RESOURCE_ID_FIELD_NAME) == resource_id_b
+    assert row_b_data.pop('field2') is None
+    assert row_b_data.pop('field3') is None
+    assert row_b_data == data_b
+
+
+def test_xlsx_multiple_separate_file(tmpdir: Path):
+    data_a = {'field1': 'A', 'field2': 'B', 'field3': 'C'}
+    data_b = {'field5': 'A', 'field1': 'B', 'field7': 'C'}
+    resource_id_a = 'resource-a'
+    resource_id_b = 'resource-b'
+    field_counts = {
+        resource_id_a: {field: 10 for field in data_a},
+        resource_id_b: {field: 10 for field in data_b},
+    }
+    request = MagicMock(separate_files=True, ignore_empty_fields=False)
+    hit = MagicMock()
+
+    with xlsx_writer(request, tmpdir, field_counts) as write:
+        write(hit, data_a, resource_id_a)
+        write(hit, data_b, resource_id_b)
+
+    for resource_id, data in [(resource_id_a, data_a), (resource_id_b, data_b)]:
+        data_file = tmpdir / f'{resource_id}.xlsx'
+        assert data_file.exists()
+
+        workbook = load_workbook(filename=data_file)
+        rows = list(workbook[DEFAULT_SHEET_NAME].rows)
+        assert len(rows) == 2
+
+        header = list(cell.value for cell in rows[0])
+        expected_headers = sorted(data)
+        assert header == expected_headers
+
+        row = rows[1]
         row_data = dict(zip(header, [cell.value for cell in row]))
-        assert row_data.pop(RESOURCE_ID_FIELD_NAME) == resource_id
         assert row_data == data
