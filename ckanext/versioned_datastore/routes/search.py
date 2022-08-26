cdf88a0809465dec57342f28c546ed80ab63e8bf
Ginger Butcher <alycejenni@gmail.com>
2022-08-26 11:38:47 +0100

    Add slug usage over time so it can be graphed

--- ckanext/versioned_datastore/routes/search.py
+++ ckanext/versioned_datastore/routes/search.py
@@ -1,10 +1,15 @@
-from ckan.plugins import toolkit
+import base64
+import json
+import zlib
+
 from ckan.model import Session
+from ckan.plugins import toolkit
 from flask import Blueprint
-from ..model.slugs import DatastoreSlug
+from sqlalchemy import func
+
 from ..lib.query import slugs
 from ..lib.query.slug_words import list_one, list_two, list_three
-
+from ..model.slugs import DatastoreSlug
 
 blueprint = Blueprint(name='search', import_name=__name__)
 
@@ -34,6 +39,20 @@ def slug_generator():
 
     reserved_slugs = slug_q.filter(DatastoreSlug.reserved_pretty_slug.isnot(None)).count()
     extra_vars['reserved'] = reserved_slugs
+
+    date_func = func.date_trunc('day', DatastoreSlug.created)
+    q = Session.query(date_func.label('date'), func.count().label('count'))
+    q = q.order_by(date_func).group_by(date_func)
+    slug_stats = []
+    total_slugs = 0
+    for stat in q.all():
+        total_slugs += stat.count
+        formatted_date = stat.date.strftime('%Y-%m-%d')
+        slug_stats.append([formatted_date, total_slugs])
+    extra_vars['slug_stats'] = slug_stats
+    # also compress the data and add to toolkit.c
+    toolkit.c.date_interval = 'day'
+    toolkit.c.graph_data = base64.b64encode(zlib.compress(json.dumps(slug_stats).encode(), level=9))
 
     attempts = 0
     while attempts < 100:
