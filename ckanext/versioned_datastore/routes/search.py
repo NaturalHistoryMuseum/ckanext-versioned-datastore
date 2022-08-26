18c69fab0345e199b569c8758e4259e3cb8b3078
Ginger Butcher <alycejenni@gmail.com>
2022-08-26 11:06:22 +0100

    Refactor slugerator route method slightly

--- ckanext/versioned_datastore/routes/search.py
+++ ckanext/versioned_datastore/routes/search.py
@@ -2,7 +2,8 @@ from ckan.plugins import toolkit
 from ckan.model import Session
 from flask import Blueprint
 from ..model.slugs import DatastoreSlug
-from ..lib.query import slug_words, slugs
+from ..lib.query import slugs
+from ..lib.query.slug_words import list_one, list_two, list_three
 
 
 blueprint = Blueprint(name='search', import_name=__name__)
@@ -20,22 +21,28 @@ def slug_generator():
     A queryless slug generator. Because why not.
     :return:
     '''
-    pretty_slugs = Session.query(DatastoreSlug).filter(DatastoreSlug.pretty_slug.isnot(None)).count()
-    reserved_slugs = Session.query(DatastoreSlug).filter(DatastoreSlug.reserved_pretty_slug.isnot(None)).count()
-    slug_combinations = len(slug_words.list_one) * len(slug_words.list_two) * len(slug_words.list_three)
+    extra_vars = {}
+    slug_q = Session.query(DatastoreSlug)
+
+    total_permutations = len(list_one) * len(list_two) * len(list_three)
+    extra_vars['total'] = total_permutations
+
+    pretty_slugs = slug_q.filter(DatastoreSlug.pretty_slug.isnot(None)).count()
+    extra_vars['unreserved'] = pretty_slugs
+    extra_vars['remaining'] = total_permutations - pretty_slugs
+    extra_vars['percent_used'] = round((pretty_slugs / total_permutations) * 100, 2)
+
+    reserved_slugs = slug_q.filter(DatastoreSlug.reserved_pretty_slug.isnot(None)).count()
+    extra_vars['reserved'] = reserved_slugs
+
     attempts = 0
     while attempts < 100:
         new_slug = slugs.generate_pretty_slug()
         if slugs.resolve_slug(new_slug) is None:
-            break
+            extra_vars['new_slug'] = new_slug
         attempts += 1
         new_slug = None  # reset if it didn't work
-    extra_vars = {
-        'new_slug': new_slug or 'something-something-something',  # default to words not on any list
-        'unreserved': pretty_slugs,
-        'reserved': reserved_slugs,
-        'total_combinations': slug_combinations,
-        'remaining': slug_combinations - pretty_slugs,
-        'percent_used': round((pretty_slugs / slug_combinations) * 100, 2)
-    }
+    # default to words not on any list if not already set
+    extra_vars['new_slug'] = extra_vars.get('new_slug', 'something-something-something')
+
     return toolkit.render('search/slugerator.html', extra_vars=extra_vars)
