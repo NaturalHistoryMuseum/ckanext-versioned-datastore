dbf55ed73f4451ffd9dfcf9b95190d8664438c84
Ginger Butcher <alycejenni@gmail.com>
2022-09-01 13:23:51 +0100

    Add friendly text for download status

--- ckanext/versioned_datastore/routes/status.py
+++ ckanext/versioned_datastore/routes/status.py
@@ -1,6 +1,6 @@
-from ckan.model import Session
-from ckan.plugins import toolkit
 from flask import Blueprint
+
+from ckan.plugins import toolkit
 from ..model.downloads import DownloadRequest
 
 blueprint = Blueprint(name='datastore_status', import_name=__name__, url_prefix='/status')
@@ -10,7 +10,18 @@ blueprint = Blueprint(name='datastore_status', import_name=__name__, url_prefix=
 def download_status(download_id):
     dl = DownloadRequest.get(download_id)
     res_show = toolkit.get_action('resource_show')
-    resources = {k: res_show({}, {'id': k}) for k in dl.core_record.resource_ids_and_versions} if dl.core_record else {}
+    resources = {k: res_show({}, {'id': k}) for k in
+                 dl.core_record.resource_ids_and_versions} if dl.core_record else {}
+
+    status_friendly = {
+        DownloadRequest.state_initial: toolkit._('Initialising'),
+        DownloadRequest.state_core_gen: toolkit._('Searching resources'),
+        DownloadRequest.state_derivative_gen: toolkit._('Generating files'),
+        DownloadRequest.state_retrieving: toolkit._('Retrieving files'),
+        DownloadRequest.state_complete: toolkit._('Complete'),
+        DownloadRequest.state_failed: toolkit._('Failed')
+    }
 
     return toolkit.render('status/download.html',
-                          extra_vars={'download_request': dl, 'resources': resources})
+                          extra_vars={'download_request': dl, 'resources': resources,
+                                      'status_friendly': status_friendly[dl.state]})
