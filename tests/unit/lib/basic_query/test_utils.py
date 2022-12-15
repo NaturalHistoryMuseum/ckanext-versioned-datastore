47b5903d907a04f08deb126c42e4315d1d94cc1b
Ginger Butcher <alycejenni@gmail.com>
2022-12-15 13:44:32 +0000

    tests: add new tables in with_versioned_datastore_tables fixture

--- tests/unit/lib/basic_query/test_utils.py
+++ tests/unit/lib/basic_query/test_utils.py
@@ -16,7 +16,9 @@ def with_versioned_datastore_tables(reset_db):
         stats.import_stats_table,
         slugs.datastore_slugs_table,
         details.datastore_resource_details_table,
-        downloads.datastore_downloads_table,
+        downloads.datastore_downloads_core_files_table,
+        downloads.datastore_downloads_derivative_files_table,
+        downloads.datastore_downloads_requests_table,
     ]
     # create the tables if they don't exist
     for table in tables:
