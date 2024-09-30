752633cc384c2d31dbfa30d0e8ebc35812f8019d
Josh Humphries <j.humphries@nhm.ac.uk>
2024-09-30 19:00:44 +0100

    fix: create migration for removal of not null constraint on stats version
    Didn't realise I had removed this but it makes sense that I did given the changes that have been made to importing. Here's the corresponding migration.

---
+++ ckanext/versioned_datastore/migration/versioned_datastore/versions/5932a36b7cf3_remove_version_not_null_constraint.py
@@ -0,0 +1,22 @@
+"""
+Remove version not null constraint.
+
+Revision ID: 5932a36b7cf3
+Revises: d2ca5da0573f
+Create Date: 2024-09-30 18:55:43.328542
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "5932a36b7cf3"
+down_revision = "d2ca5da0573f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column("versioned_datastore_import_stats", "version", nullable=True)
+
+
+def downgrade():
+    op.alter_column("versioned_datastore_import_stats", "version", nullable=False)
