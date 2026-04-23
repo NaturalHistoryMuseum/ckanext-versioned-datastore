NaturalHistoryMuseum/ckanext-versioned-datastore

commit ba0a3dcdff543eadfbe7316e33b86a66dd58ea9d

perf: cache list of public resources
Most resources are going to be public, so instead of retrieving a new list every time, or checking every single resource individually, we can cache a list of public resources and their status in the datastore. The cache is cleared every time a package or resource is created, updated, or deleted.

ckanext/versioned_datastore/plugin.py
@@ -2,6 +2,7 @@ import logging
 from contextlib import suppress
 from typing import List, Optional
 
+from beaker.cache import cache_regions
 from ckan.plugins import (
     SingletonPlugin,
     implements,
@@ -22,6 +23,7 @@ from ckanext.versioned_datastore.lib.tasks import get_es_health, get_queue_lengt
 from ckanext.versioned_datastore.lib.utils import (
     RawResourceException,
     ReadOnlyResourceException,
+    clear_cached_metadata,
     iqs_implementations,
     is_datastore_resource,
     ivds_implementations,
@@ -104,6 +106,7 @@ class VersionedSearchPlugin(SingletonPlugin):
     implements(interfaces.IBlueprint, inherit=True)
     implements(IVersionedDatastoreQuerySchema)
     implements(interfaces.IClick)
+    implements(interfaces.IPackageController, inherit=True)
     if status_available:
         implements(IStatus)
 
@@ -154,6 +157,13 @@ class VersionedSearchPlugin(SingletonPlugin):
                 query = SchemaQuery(**query_parameters)
                 with suppress(Exception):
                     reserve_slug(reserved_pretty_slug, query)
+
+        # configure cache
+        options = {}
+        for k, v in ckan_config.items():
+            if k.startswith('ckanext.versioned_datastore.cache.'):
+                options[k.split('.')[-1]] = v
+        cache_regions.update({'vds': options})
 
     def is_sg_configured(self) -> bool:
         """
@@ -248,6 +258,7 @@ class VersionedSearchPlugin(SingletonPlugin):
         data_dict = {'resource_id': resource['id'], 'replace': True}
         with suppress(ReadOnlyResourceException), suppress(RawResourceException):
             toolkit.get_action('vds_data_add')(context, data_dict)
+        clear_cached_metadata()
 
     # IResourceController
     def after_create(self, context: dict, resource: dict):
@@ -255,9 +266,23 @@ class VersionedSearchPlugin(SingletonPlugin):
         data_dict = {'resource_id': resource['id'], 'replace': True}
         with suppress(ReadOnlyResourceException), suppress(RawResourceException):
             toolkit.get_action('vds_data_add')(context, data_dict)
+        clear_cached_metadata()
 
     def before_delete(self, context: dict, resource: dict, resources: List[dict]):
         toolkit.get_action('vds_data_delete')(context, {'resource_id': resource['id']})
+
+    def after_delete(self, context: dict, resources: List[dict]):
+        clear_cached_metadata()
+
+    # IPackageController
+    def after_create(self, context: dict, pkg_dict: dict):
+        clear_cached_metadata()
+
+    def after_update(self, context: dict, pkg_dict: dict):
+        clear_cached_metadata()
+
+    def after_delete(self, context: dict, pkg_dict: dict):
+        clear_cached_metadata()
 
     # IConfigurer
     def update_config(self, config):

ckanext/versioned_datastore/lib/utils.py
@@ -1,6 +1,8 @@
+import logging
 import re
-from typing import Iterable, Optional, Set, TypeVar
+from typing import Dict, Iterable, Optional, Set, TypeVar
 
+from beaker.cache import CacheManager, cache_region, cache_regions
 from ckan import plugins
 from ckan.plugins import get_plugin, toolkit
 from elasticsearch import Elasticsearch
@@ -13,6 +15,8 @@ from ckanext.versioned_datastore.interfaces import (
     IVersionedDatastoreQuerySchema,
 )
 from ckanext.versioned_datastore.lib import common
+
+log = logging.getLogger(__name__)
 
 
 def get_available_datastore_resources(
@@ -66,6 +70,31 @@ def get_available_resources(
                 for resource in package['resources']
                 if not datastore_only or resource.get('datastore_active', False)
             )
+        offset += len(packages)
+
+    return resource_ids
+
+
+@cache_region('vds', 'public_resources')
+def get_public_resources() -> Dict[str, bool]:
+    """
+    Retrieves a list of public resources and whether they are present in the datastore.
+
+    :returns: dict of publicly available resource IDs and whether they are available in
+        the datastore
+    """
+    resource_ids = {}
+    offset = 0
+    action = toolkit.get_action('current_package_list_with_resources')
+    while True:
+        # do not ignore auth, do not provide a user
+        context = {'ignore_auth': False}
+        packages = action(context, {'offset': offset, 'limit': 100})
+        if not packages:
+            break
+        for package in packages:
+            for resource in package.get('resources', []):
+                resource_ids[resource['id']] = is_datastore_resource(resource['id'])
         offset += len(packages)
 
     return resource_ids
@@ -277,3 +306,33 @@ def idownload_implementations() -> Iterable[U]:
 
 def iqs_implementations() -> Iterable[V]:
     yield from plugins.PluginImplementations(IVersionedDatastoreQuerySchema)
+
+
+def clear_cached_metadata():
+    """
+    Clears cached package and resource metadata, e.g. lists of public resource IDs.
+
+    Cached functions have to be added manually and cleared individually. This is partly
+    so we can control exactly which functions to clear, and partly because beaker does
+    not seem to have a way to clear an entire region.
+    :return:
+    """
+    cache_opts = cache_regions.get('vds')
+    if cache_opts is None:
+        # this shouldn't happen, but just in case
+        cache_opts = {}
+        for k, v in toolkit.config.items():
+            if k.startswith('ckanext.versioned_datastore.cache.'):
+                cache_opts[k.split('.')[-1]] = v
+    # cache_managers does not usually seem to be populated so just construct a new ref
+    cache_manager = CacheManager(**cache_opts)
+    # manually list the cached functions to be cleared
+    cached_functions = [get_public_resources]
+    for func in cached_functions:
+        # each function has its own namespace that needs to be cleared
+        try:
+            cache = cache_manager.get_cache(func._arg_namespace)
+            cache.clear()
+            log.info(f'Cleared cache for {func.__name__}')
+        except Exception as e:
+            log.error(f'Failed to clear cache for {func.__name__}: {e}')

ckanext/versioned_datastore/logic/slug/action.py
@@ -15,8 +15,8 @@ from ckanext.versioned_datastore.lib.query.slugs.slugs import (
     resolve_slug,
 )
 from ckanext.versioned_datastore.lib.query.utils import convert_to_multisearch
-from ckanext.versioned_datastore.lib.utils import get_available_datastore_resources
 from ckanext.versioned_datastore.logic.slug import helptext, schema
+from ckanext.versioned_datastore.logic.validators import validate_datastore_resource_ids
 
 
 @action(schema.vds_slug_create(), helptext.vds_slug_create)
@@ -153,10 +153,7 @@ def vds_slug_resolve(slug: str):
         )
 
     # check that all the resources exist and are available
-    all_resource_ids = get_available_datastore_resources(
-        ignore_auth=False, user_id=getattr(toolkit.g, 'user', '')
-    )
-    valid_resource_ids = list(set(result['resource_ids']) & all_resource_ids)
+    valid_resource_ids = validate_datastore_resource_ids(result['resource_ids'])
     valid_resource_count = len(valid_resource_ids)
     current_resource_count = len(result['resource_ids'])
     if valid_resource_count != current_resource_count:

ckanext/versioned_datastore/logic/validators.py
@@ -5,7 +5,10 @@ from typing import Iterable, Optional, Union
 from ckan.model import Resource, Session
 from ckan.plugins import toolkit
 
-from ckanext.versioned_datastore.lib.utils import is_datastore_resource
+from ckanext.versioned_datastore.lib.utils import (
+    get_public_resources,
+    is_datastore_resource,
+)
 
 boolean_validator = toolkit.get_validator('boolean_validator')
 ignore_missing = toolkit.get_validator('ignore_missing')
@@ -111,11 +114,16 @@ def validate_resource_ids(value: Union[str, list], context: Optional[dict] = Non
         raise toolkit.Invalid('Invalid list of resource ID strings')
 
     context = _populate_context(context)
-    valid_resource_ids = [
-        resource_id
-        for resource_id in _deduplicate(value)
-        if check_resource_id(resource_id, context)
-    ]
+    public_resources = get_public_resources()
+    valid_resource_ids = []
+    for resource_id in _deduplicate(value):
+        datastore_status = public_resources.get(resource_id)
+        if datastore_status is None:
+            if check_resource_id(resource_id, context):
+                valid_resource_ids.append(resource_id)
+        else:
+            valid_resource_ids.append(resource_id)
+
     if value and not valid_resource_ids:
         # the user passed some resources, but none of them were datastore resources
         raise toolkit.Invalid('No resource IDs are available')
@@ -132,23 +140,15 @@ def validate_datastore_resource_ids(
         raise toolkit.Invalid('Invalid list of resource ID strings')
 
     context = _populate_context(context)
-    get_current_packages = toolkit.get_action('current_package_list_with_resources')
-    all_available_resource_ids = []
-    offset = 0
-    page_size = 500
-    while True:
-        page = get_current_packages(context, {'limit': page_size, 'offset': offset})
-        all_available_resource_ids += [r['id'] for p in page for r in p['resources']]
-        if len(page) < page_size:
-            break
-        offset += page_size
+    public_resources = get_public_resources()
+    valid_resource_ids = []
+    for resource_id in _deduplicate(value):
+        datastore_status = public_resources.get(resource_id)
+        if datastore_status is None:
+            datastore_status = check_datastore_resource_id(resource_id, context)
+        if datastore_status:
+            valid_resource_ids.append(resource_id)
 
-    valid_resource_ids = [
-        resource_id
-        for resource_id in _deduplicate(value)
-        if resource_id in all_available_resource_ids
-        and is_datastore_resource(resource_id)
-    ]
     if value and not valid_resource_ids:
         # the user passed some resources, but none of them were datastore resources
         raise toolkit.Invalid('No resource IDs are datastore resources')

commit 28005d88ba77d6b9741bacfe8968b4496fc050af

fix: handle overlapping methods
Fixes an issue introduced in ba0a3dc where identically named methods from IPackageController and IResourceController were both used, meaning the IResourceController methods were being ignored. This renames the methods for 2.10+ compatibility, and makes the old names handle both interfaces (not ideal, but we still need to support 2.9).

ckanext/versioned_datastore/plugin.py
@@ -216,8 +216,77 @@ class VersionedSearchPlugin(SingletonPlugin):
             'nav_slug': helpers.nav_slug,
         }
 
-    # IResourceController
+    # IResourceController (<2.10 compatibility)
+    def before_create(self, context, resource):
+        # only in IResourceController, but renamed
+        self.before_resource_create(context, resource)
+
+    # IResourceController and IPackageController (<2.10 compatibility)
+    def after_create(self, context, data_dict=None, resource=None, pkg_dict=None):
+        data_dict = data_dict or resource or pkg_dict
+        is_resource = (resource is not None) or ('resource_type' in data_dict)
+        if is_resource:
+            self.after_resource_create(context, data_dict)
+        else:
+            self.after_dataset_create(context, data_dict)
+
+    # IResourceController (<2.10 compatibility)
     def before_show(self, resource_dict):
+        # only in IResourceController, but renamed
+        self.before_resource_show(resource_dict)
+
+    # IResourceController (<2.10 compatibility)
+    def before_update(self, context, current, resource):
+        # only in IResourceController, but renamed
+        self.before_resource_update(context, current, resource)
+
+    # IResourceController and IPackageController (<2.10 compatibility)
+    def after_update(self, context, data_dict=None, resource=None, pkg_dict=None):
+        data_dict = data_dict or resource or pkg_dict
+        is_resource = (resource is not None) or ('resource_type' in data_dict)
+        if is_resource:
+            self.after_resource_update(context, data_dict)
+        else:
+            self.after_dataset_update(context, data_dict)
+
+    # IResourceController (<2.10 compatibility)
+    def before_delete(self, context: dict, resource: dict, resources: List[dict]):
+        # only in IResourceController, but renamed
+        self.before_resource_delete(context, resource, resources)
+
+    # IResourceController and IPackageController (<2.10 compatibility)
+    def after_delete(self, context, data_obj=None, resources=None, pkg_dict=None):
+        data_obj = data_obj or resources or pkg_dict
+        is_resource = (resources is not None) or isinstance(data_obj, list)
+        if is_resource:
+            self.after_resource_delete(context, data_obj)
+        else:
+            self.after_dataset_delete(context, data_obj)
+
+    # IResourceController
+    def before_resource_create(self, context, resource):
+        # only set disable_parsing if it's True (False by default)
+        if toolkit.asbool(resource.pop('disable_parsing', False)):
+            resource['disable_parsing'] = True
+        # same for download_original_filenames
+        if toolkit.asbool(resource.pop('download_original_filenames', False)):
+            resource['download_original_filenames'] = True
+
+    # IResourceController
+    def after_resource_create(self, context: dict, resource: dict):
+        # use replace to overwrite the existing data (this is what users would expect)
+        data_dict = {'resource_id': resource['id'], 'replace': True}
+        with suppress(utils.ReadOnlyResourceException), suppress(
+            utils.RawResourceException
+        ):
+            toolkit.get_action('vds_data_add')(context, data_dict)
+        try:
+            clear_cache_region('versioned_datastore', utils, cache_name='vds')
+        except CacheClearError as e:
+            log.error(e)
+
+    # IResourceController
+    def before_resource_show(self, resource_dict):
         # ensure datastore_active is set where it should be
         resource_dict['datastore_active'] = utils.is_datastore_resource(
             resource_dict['id']
@@ -228,16 +297,7 @@ class VersionedSearchPlugin(SingletonPlugin):
         return resource_dict
 
     # IResourceController
-    def before_create(self, context, resource):
-        # only set disable_parsing if it's True (False by default)
-        if toolkit.asbool(resource.pop('disable_parsing', False)):
-            resource['disable_parsing'] = True
-        # same for download_original_filenames
-        if toolkit.asbool(resource.pop('download_original_filenames', False)):
-            resource['download_original_filenames'] = True
-
-    # IResourceController
-    def before_update(self, context, current, resource):
+    def before_resource_update(self, context, current, resource):
         # we can't automatically go from ingested to raw because it might be included
         # in queries and DOIs, but we can ingest a file that was previously raw
         was_raw = toolkit.asbool(current.get('disable_parsing', False))
@@ -250,7 +310,7 @@ class VersionedSearchPlugin(SingletonPlugin):
             resource['download_original_filenames'] = True
 
     # IResourceController
-    def after_update(self, context: dict, resource: dict):
+    def after_resource_update(self, context: dict, resource: dict):
         # use replace to overwrite the existing data (this is what users would expect)
         data_dict = {'resource_id': resource['id'], 'replace': True}
         with suppress(utils.ReadOnlyResourceException), suppress(
@@ -263,41 +323,34 @@ class VersionedSearchPlugin(SingletonPlugin):
             log.error(e)
 
     # IResourceController
-    def after_create(self, context: dict, resource: dict):
-        # use replace to overwrite the existing data (this is what users would expect)
-        data_dict = {'resource_id': resource['id'], 'replace': True}
-        with suppress(utils.ReadOnlyResourceException), suppress(
-            utils.RawResourceException
-        ):
-            toolkit.get_action('vds_data_add')(context, data_dict)
-        try:
-            clear_cache_region('versioned_datastore', utils, cache_name='vds')
-        except CacheClearError as e:
-            log.error(e)
-
-    def before_delete(self, context: dict, resource: dict, resources: List[dict]):
+    def before_resource_delete(
+        self, context: dict, resource: dict, resources: List[dict]
+    ):
         toolkit.get_action('vds_data_delete')(context, {'resource_id': resource['id']})
 
-    def after_delete(self, context: dict, resources: List[dict]):
+    # IResourceController
+    def after_resource_delete(self, context: dict, resources: List[dict]):
         try:
             clear_cache_region('versioned_datastore', utils, cache_name='vds')
         except CacheClearError as e:
             log.error(e)
 
     # IPackageController
-    def after_create(self, context: dict, pkg_dict: dict):
+    def after_dataset_create(self, context: dict, pkg_dict: dict):
         try:
             clear_cache_region('versioned_datastore', utils, cache_name='vds')
         except CacheClearError as e:
             log.error(e)
 
-    def after_update(self, context: dict, pkg_dict: dict):
+    # IPackageController
+    def after_dataset_update(self, context: dict, pkg_dict: dict):
         try:
             clear_cache_region('versioned_datastore', utils, cache_name='vds')
         except CacheClearError as e:
             log.error(e)
 
-    def after_delete(self, context: dict, pkg_dict: dict):
+    # IPackageController
+    def after_dataset_delete(self, context: dict, pkg_dict: dict):
         try:
             clear_cache_region('versioned_datastore', utils, cache_name='vds')
         except CacheClearError as e: